bccf59c97867ebf8baae7a76d5ea701b5bc0e09c
Elbeard <elektrobeard@gmail.com>
2018-02-02 01:58:53 +0700

    Небольшие успехи с выводом ошибок. На данном этапе программа выдаёт         ошибку 20 (BASS_ERROR_ILLPARAM The value content or size is not valid.         some attributes may have additional error codes, see the documentation.)

--- player.cpp
+++ player.cpp
@@ -1,29 +1,46 @@
 #include "player.h"
 #include "bass.h"
 #include <QMessageBox>
-//#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 
 Player::Player(QObject *parent) : QObject(parent)
 {
     filename = "C:/Qt/QtProg/5.mp3";
 
-    player->Initialize();
-    player->Error("message in main");
+    this->Initialize();
 
     /*    HSTREAM stream=BASS_StreamCreateFile(FALSE, filename, 0, 0,BASS_SAMPLE_LOOP);
         if(BASS_ErrorGetCode() != BASS_OK){
-            player->Error("Stream error\n");
+            this->Error("Stream error\n");
         }
 
         // play the music (continue from current position)
         if (!BASS_ChannelPlay(stream,FALSE))
-            player->Error("Can't play music");
+            this->Error("Can't play music");
      */
 }
 
 void Player::Play()
 {
-    this->Error("PlayButton clicked"); // test PlayButton
+    //this->Error("PlayButton clicked"); // test PlayButton
+
+    //stream = BASS_StreamCreateFile(TRUE, "c:/3.mp3", 0, 0,BASS_SAMPLE_LOOP);
+
+    if (!BASS_StreamCreateFile(TRUE, "C:/3.mp3", 0, 0,BASS_SAMPLE_LOOP))
+
+        //Здесь маленькая победа с преобразованием топов. (Решение заняло два вечера.)
+        this->Error(QString::number(BASS_ErrorGetCode()));
+
+//    if(BASS_ErrorGetCode() != BASS_OK){
+//        this->Error("Stream error\n");
+//    }
+
+       // play the music (continue from current position)
+       if (!BASS_ChannelPlay(stream,FALSE))
+           this->Error("Can't play music");
 }
 
 void Player::Error(QString text)
@@ -46,11 +63,10 @@ void Player::Initialize()
     //  output device
       if (!BASS_Init(-1,44100,0,0,NULL))
          this->Error("Can't initialize device");
-      else {
+     /* else {
          this->Error("BASS is successful initialaze\n");
-     }
+     }*/
 
-     player->Error("init message");
 }
 
 Player::~Player()
